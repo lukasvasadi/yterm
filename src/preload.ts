95430349807d7eaf2f888553b10b41226959e59a
lukasvasadi <lukas.vasadi@gmail.com>
2022-12-01 16:16:38 -0500

    First commit

--- src/preload.ts
+++ src/preload.ts
@@ -1,14 +1,24 @@
-// All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
+import { ipcRenderer, contextBridge } from "electron"
+
+contextBridge.exposeInMainWorld("api", {
+  getPorts: () => ipcRenderer.invoke("get-ports"),
+  setPort: (port: any) => ipcRenderer.invoke("set-port", port),
+  getData: () => ipcRenderer.on("data", (data) => console.log(data)),
+  sendData: (data: string) => ipcRenderer.invoke("send-data", data),
+})
+
 window.addEventListener("DOMContentLoaded", () => {
   const replaceText = (selector: string, text: string) => {
-    const element = document.getElementById(selector);
+    const element = document.getElementById(selector)
     if (element) {
-      element.innerText = text;
+      element.innerText = text
     }
-  };
+  }
 
   for (const type of ["chrome", "node", "electron"]) {
-    replaceText(`${type}-version`, process.versions[type as keyof NodeJS.ProcessVersions]);
+    replaceText(
+      `${type}-version`,
+      process.versions[type as keyof NodeJS.ProcessVersions]
+    )
   }
-});
+})
--- src/renderer.ts
+++ src/renderer.ts
@@ -1,6 +1,33 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process unless
-// nodeIntegration is set to true in webPreferences.
-// Use preload.js to selectively enable features
-// needed in the renderer process.
+console.log("Hello from renderer process")
+
+// @ts-expect-error
+api.getPorts().then((ports: any) => {
+  var select: any = document.getElementById("ports")
+  console.log(ports)
+  for (var key in ports) {
+    console.log(ports[key])
+    if (ports[key].manufacturer) {
+      var option = document.createElement("option")
+      option.text = ports[key].path + " | " + ports[key].manufacturer
+      select.add(option)
+    }
+  }
+})
+
+document.getElementById("connect").onclick = () => {
+  // @ts-expect-error
+  var path = document.getElementById("ports").value
+  path = path.substring(0, path.indexOf(" "))
+  // @ts-expect-error
+  var baudrate = document.getElementById("baudrate").value
+  console.log({ path: path, baudrate: parseInt(baudrate) })
+  // @ts-expect-error
+  api.setPort({ path: path, baudrate: parseInt(baudrate) })
+}
+
+document.getElementById("send").onclick = () => {
+  // @ts-expect-error
+  var message = document.getElementById("data").value
+  // @ts-expect-error
+  api.sendData(message)
+}
